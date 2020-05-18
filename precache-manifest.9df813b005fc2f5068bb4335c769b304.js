2bc4b7588d79ad7c54420cd01d10f8fd09adea2d
kumarvadivel <35343907+kumarvadivel@users.noreply.github.com>
2020-05-18 19:05:59 +0530

    Updates

---
+++ precache-manifest.9df813b005fc2f5068bb4335c769b304.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d36e531893b5daaa3974fe5d7e3fa42",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "484f7838827945d0eed8",
+    "url": "/login/static/css/main.8946f807.chunk.css"
+  },
+  {
+    "revision": "5030ebffd3c8e2a3248e",
+    "url": "/login/static/js/2.e84b3f6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/login/static/js/2.e84b3f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484f7838827945d0eed8",
+    "url": "/login/static/js/main.b6ef4a5d.chunk.js"
+  },
+  {
+    "revision": "b802f2991cc3d33cba5f",
+    "url": "/login/static/js/runtime-main.fa65eb93.js"
+  }
+]);
